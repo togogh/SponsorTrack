f17f189604450a4099d75b8a21462388a513d83f
Ysabel Caballes <5388340+togogh@users.noreply.github.com>
2025-05-26 16:11:41 +0700

    modify app layout & update screenshot

--- src/sponsortrack/frontend/app.py
+++ src/sponsortrack/frontend/app.py
@@ -2,45 +2,70 @@ import gradio as gr
 from sponsortrack.backend.video import Video
 
 
-def submit(url, segments):
-    segments_copy = segments.copy()
+def submit(url):
     video = Video(url)
     segments_info = video.fetch_segments_info()
-    segments_copy.extend(segments_info)
-    return "", segments_copy
+    return "", segments_info
 
 
 with gr.Blocks() as demo:
-    url = gr.Textbox(label="Youtube URL")
-    submit_btn = gr.Button("Get Sponsor Info")
-    segments = gr.State([])
-    submit_btn.click(fn=submit, inputs=[url, segments], outputs=[url, segments], api_name="submit")
+    with gr.Row():
+        gr.Markdown("""
+# SponsorTrack
+Plug in a Youtube video and get who sponsored it and what they're offering
+        """)
 
-    @gr.render(inputs=segments)
-    def render_count(arr):
-        if len(arr) != 0:
-            gr.Markdown(
-                f"Found {len(arr)} sponsored segment{'' if len(arr) == 1 else 's'} for https://www.youtube.com/watch?v={arr[0]['parent_video_id']}"
+    with gr.Row():
+        with gr.Column(scale=1):
+            url = gr.Textbox(
+                label="Youtube URL", placeholder="https://www.youtube.com/watch?v=XXXXXXXX"
             )
-            for i, segment in enumerate(arr):
-                with gr.Accordion(label=f"{segment['sponsor_info']['sponsor_name']}", open=False):
-                    gr.HTML(f"""
-                        <iframe
-                            width="560"
-                            height="315"
-                            src="https://www.youtube.com/embed/{segment["parent_video_id"]}?start={int(segment["start_time"])}"
-                            frameborder="0"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                            referrerpolicy="strict-origin-when-cross-origin"
-                            allowfullscreen>
-                        </iframe>""")
-                    gr.TextArea(
-                        label="Description", value=segment["sponsor_info"]["sponsor_description"]
+            examples = gr.Examples(
+                examples=[
+                    "https://www.youtube.com/watch?v=fZbXLmKqkM4",
+                    "https://youtu.be/ofKe4b169ts",
+                    "https://youtu.be/NxCfacrnbFU",
+                    "https://www.youtube.com/watch?v=AienZMjqAg4&t=424s",
+                ],
+                inputs=[url],
+            )
+            submit_btn = gr.Button("Get Sponsor Info", variant="primary")
+            segments = gr.State([])
+            submit_btn.click(fn=submit, inputs=[url], outputs=[url, segments], api_name="submit")
+
+        with gr.Column(scale=2):
+
+            @gr.render(inputs=segments)
+            def render_count(arr):
+                if len(arr) != 0:
+                    gr.Markdown(
+                        f"Found {len(arr)} sponsored segment{'' if len(arr) == 1 else 's'} for https://www.youtube.com/watch?v={arr[0]['parent_video_id']}"
                     )
-                    gr.Textbox(label="Offer", value=segment["sponsor_info"]["sponsor_offer"])
-                    gr.Textbox(
-                        label="Links", value=", ".join(segment["sponsor_info"]["sponsor_links"])
-                    )
+                    for i, segment in enumerate(arr):
+                        with gr.Accordion(
+                            label=f"{segment['sponsor_info']['sponsor_name']}", open=False
+                        ):
+                            gr.HTML(f"""
+                                <iframe
+                                    width="560"
+                                    height="315"
+                                    src="https://www.youtube.com/embed/{segment["parent_video_id"]}?start={int(segment["start_time"])}"
+                                    frameborder="0"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                    referrerpolicy="strict-origin-when-cross-origin"
+                                    allowfullscreen>
+                                </iframe>""")
+                            gr.TextArea(
+                                label="Description",
+                                value=segment["sponsor_info"]["sponsor_description"],
+                            )
+                            gr.Textbox(
+                                label="Offer", value=segment["sponsor_info"]["sponsor_offer"]
+                            )
+                            gr.Textbox(
+                                label="Links",
+                                value=", ".join(segment["sponsor_info"]["sponsor_links"]),
+                            )
 
 
 demo.queue().launch(show_error=True)
