0be1678c328d1c32ed5e129021849721a8eddd3a
Ysabel Caballes <5388340+togogh@users.noreply.github.com>
2025-06-07 16:20:59 +0700

    feature: add user and accesstoken models

---
+++ src/sponsortrack/models/user.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, String
+from sponsortrack.models.base import Base
+from sqlalchemy.orm import relationship
+
+
+class User(Base):
+    email = Column(String, nullable=False)
+    username = Column(String, unique=True, nullable=False)
+    pasword = Column(String, nullable=False)
+    access_token = relationship("AccessToken", back_populates="user")
