c42d3d89e1fc875f272b380b82fce0d99cec23dd
Ysabel Caballes <5388340+togogh@users.noreply.github.com>
2025-06-08 21:34:21 +0700

    feat: add is_superuser and is_verified to user

--- src/sponsortrack/backend/db/models/user.py
+++ src/sponsortrack/backend/db/models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Boolean
 from sponsortrack.backend.db.models.base import Base
 from sqlalchemy.orm import relationship
 
@@ -8,3 +8,5 @@ class User(Base):
     username = Column(String, unique=True, nullable=False)
     password = Column(String, nullable=False)
     access_tokens = relationship("AccessToken", back_populates="user")
+    is_superuser = Column(Boolean, nullable=False, default=False)
+    is_verified = Column(Boolean, nullable=False, default=False)
