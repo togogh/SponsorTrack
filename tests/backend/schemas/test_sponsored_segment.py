e00849385f7024f6f2824d6150a223eee149b9a3
Ysabel Caballes <5388340+togogh@users.noreply.github.com>
2025-07-08 20:11:36 +0700

    feat: remove duration from test_validate_times

--- tests/backend/schemas/test_sponsored_segment.py
+++ tests/backend/schemas/test_sponsored_segment.py
@@ -4,25 +4,20 @@ import pytest
 
 
 @pytest.mark.parametrize(
-    "start_time, end_time, duration, expected_error",
+    "start_time, end_time, expected_error",
     [
-        pytest.param(10, 15, 5, None),
-        pytest.param(10, 15, 3, ValidationError),
-        pytest.param(15, 10, 5, ValidationError),
-        pytest.param(None, None, None, None),
-        pytest.param(None, 5, 3, None),
-        pytest.param(1, 5, None, None),
-        pytest.param(19, 5, None, ValidationError),
+        pytest.param(10, 15, None),
+        pytest.param(None, None, None),
+        pytest.param(None, 5, None),
+        pytest.param(1, None, None),
+        pytest.param(19, 5, ValidationError),
     ],
 )
-async def test_validate_times(start_time, end_time, duration, expected_error):
+async def test_validate_times(start_time, end_time, expected_error):
     if expected_error is not None:
         with pytest.raises(expected_error):
-            SponsoredSegmentUpdate(start_time=start_time, end_time=end_time, duration=duration)
+            SponsoredSegmentUpdate(start_time=start_time, end_time=end_time)
     else:
-        sponsored_segment = SponsoredSegmentUpdate(
-            start_time=start_time, end_time=end_time, duration=duration
-        )
+        sponsored_segment = SponsoredSegmentUpdate(start_time=start_time, end_time=end_time)
         assert start_time == sponsored_segment.start_time
         assert end_time == sponsored_segment.end_time
-        assert duration == sponsored_segment.duration
