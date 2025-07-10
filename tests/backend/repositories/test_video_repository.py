2ca27a885d4f534afe167b14d9ab8f8245b60f24
Ysabel Caballes <5388340+togogh@users.noreply.github.com>
2025-07-10 22:08:17 +0700

    feat: finish test video repository

--- tests/backend/repositories/test_video_repository.py
+++ tests/backend/repositories/test_video_repository.py
@@ -1,5 +1,6 @@
 from backend.repositories.all import VideoRepository
-from backend.schemas.all import VideoCreate
+from backend.schemas.all import VideoCreate, VideoUpdate
+from backend.models.all import Video
 import pytest
 from sqlalchemy.exc import IntegrityError
 from datetime import datetime
@@ -71,3 +72,79 @@ async def test_add_full(test_session, repo, base_fields, video_fields):
             assert getattr(video, k) == datetime.strptime(v, "%Y-%m-%d").date()
         else:
             assert getattr(video, k) == v
+
+
+@pytest.mark.asyncio(loop_scope="session")
+async def test_get_by_id(test_session, repo):
+    video = await repo.get_by_id("2df9f22f-a2e1-4498-940e-41c105a05f41", test_session)
+    assert video is None
+
+    video = await repo.get_by_youtube_id("non-existent", test_session)
+    assert video is None
+
+    youtube_id = "On2V_L9jwS4"
+    video_data = {"youtube_id": youtube_id}
+    video_create = VideoCreate(**video_data)
+    video = await repo.add(video_create, test_session)
+    id = video.id
+
+    video = await repo.get_by_youtube_id(youtube_id, test_session)
+    assert isinstance(video, Video)
+    assert video.youtube_id == youtube_id
+
+    video = await repo.get_by_id(id, test_session)
+    assert isinstance(video, Video)
+    assert video.id == id
+
+
+@pytest.mark.asyncio(loop_scope="session")
+async def test_update(test_session, repo, base_fields, video_fields):
+    retained_fields = ["id", "youtube_id", "created_at"]
+    all_fields = base_fields + video_fields
+    changed_fields = [field for field in all_fields if field not in retained_fields]
+
+    id = "a6945747-6dab-429e-a2cf-4c3d2f0e0727"
+    await repo.update(id, VideoUpdate(language="en"), test_session)
+    updated_video = await repo.get_by_id(id, test_session)
+    assert updated_video is None
+
+    original_video_data = {
+        "youtube_id": "wyo1u9WxUG4",
+        "language": "en",
+        "title": "test title",
+        "upload_date": "2025-06-24",
+        "description": "test description",
+        "duration": 12,
+        "channel": "another_channel",
+    }
+    video_create = VideoCreate(**original_video_data)
+    added_video = await repo.add(video_create, test_session)
+    added_video_data = {k: v for k, v in added_video.__dict__.items() if k in all_fields}
+    added_video_copy = Video(**added_video_data)
+
+    new_video_data = {
+        "language": "dc",
+        "title": "new title",
+        "upload_date": "2025-06-26",
+        "description": "test description updated",
+        "duration": 16,
+        "channel": "more_channel",
+    }
+    video_update = VideoUpdate(**new_video_data)
+    await repo.update(added_video.id, video_update, test_session)
+
+    updated_video = await repo.get_by_id(added_video.id, test_session)
+    await test_session.refresh(updated_video)
+    assert isinstance(updated_video, Video)
+
+    for field in retained_fields:
+        assert getattr(added_video_copy, field) == getattr(updated_video, field)
+    for field in changed_fields:
+        assert getattr(added_video_copy, field) != getattr(updated_video, field)
+        if field == "upload_date":
+            assert (
+                getattr(updated_video, field)
+                == datetime.strptime(new_video_data[field], "%Y-%m-%d").date()
+            )
+        elif field != "updated_at":
+            assert getattr(updated_video, field) == new_video_data[field]
