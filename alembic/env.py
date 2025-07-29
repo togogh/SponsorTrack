1d62ef6fa4b4013dc211ebf5d09ccc8588b4b7d0
Ysabel Caballes <5388340+togogh@users.noreply.github.com>
2025-07-29 20:20:45 +0700

    feat: make alembic work

--- alembic/env.py
+++ alembic/env.py
@@ -1,8 +1,8 @@
 from logging.config import fileConfig
-
 from backend.core.session import get_engine
 from backend.models.all import Base
-
+from backend.core.settings import db_settings
+from sqlalchemy import MetaData
 from alembic import context
 import asyncio
 from backend.logs.config import get_logger
@@ -23,13 +23,19 @@ if config.config_file_name is not None:
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
-target_metadata = Base.metadata
+# target_metadata = Base.metadata
 # target_metadata = None
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
+
+
+def include_name(object, name, type_):
+    if type_ == "schema" and name == db_settings.POSTGRES_PROD_SCHEMA:
+        return True
+    return False
 
 
 def run_migrations_offline() -> None:
@@ -54,16 +60,29 @@ async def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
+    translated = MetaData()
+
+    def translate_schema(table, to_schema, constraint, referred_schema):
+        return to_schema
+
+    for table in Base.metadata.tables.values():
+        table.tometadata(
+            translated,
+            schema=db_settings.POSTGRES_PROD_SCHEMA if table.schema == "env" else table.schema,
+            referred_schema_fn=translate_schema,
+        )
+
     async with get_engine() as engine:
         async with engine.connect() as connection:
             await connection.run_sync(
                 lambda sync_conn: context.configure(
                     connection=sync_conn,
-                    target_metadata=target_metadata,
+                    target_metadata=translated,
                     compare_type=True,
                     include_schemas=True,
                     version_table_schema="public",
                     compare_server_default=True,
+                    include_name=include_name,
                 )
             )
 
